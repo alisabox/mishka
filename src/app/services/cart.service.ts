088a1eed5ee37a84126b075b91ab13393c370b1b
Alisa <alisabox@mail.ru>
2023-01-08 01:57:15 +0900

    fix: update cart service

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -10,11 +10,15 @@ export interface CartProduct {
   providedIn: 'root',
 })
 export class CartService {
-
   private _products: CartProduct[] = [];
+
+  constructor() {
+    this._products = JSON.parse(localStorage.getItem('products') || '[]');
+  }
 
   public addToCart(product: Product): void {
     const addedProduct = this._products.find(item => item.product.id === product.id);
+
     if (addedProduct) {
       addedProduct.quantity += 1;
     } else {
@@ -23,6 +27,8 @@ export class CartService {
         quantity: 1,
       });
     }
+
+    this._syncProducts();
   }
 
   public updateQuantity(productId: string | number, newQuantity: number): void {
@@ -31,13 +37,15 @@ export class CartService {
     if (updatedProduct) {
       updatedProduct.quantity = newQuantity;
     }
+
+    this._syncProducts();
   }
 
   public removeProduct(productId: string | number): void {
     const index = this._products.findIndex(item => item.product.id === productId);
     this._products = [...this._products.slice(0, index), ...this._products.slice(index + 1)];
-    // eslint-disable-next-line no-console
-    console.log(this._products);
+
+    this._syncProducts();
   }
 
   public getItems(): CartProduct[] {
@@ -46,5 +54,10 @@ export class CartService {
 
   public clearCart(): void {
     this._products = [];
+    this._syncProducts();
+  }
+
+  private _syncProducts(): void {
+    localStorage.setItem('products', JSON.stringify(this._products));
   }
 }
